88117969fc05a730777787a7abcab0b4c2d7fb52
Diego Gentner <dgentner@ucol.mx>
2020-11-26 15:13:54 -0600

    Added convertir function

--- src/main.js
+++ src/main.js
@@ -1,5 +1,30 @@
 export default class App {
 
+convertir(pesos) {
+    let resultado;
+    if (pesos < 0) {
+        resultado = 0
+    } else {
+        if (pesos < 2000) {
+            resultado = pesos * 20.5;
+        } else {
+            resultado = pesos * 22.7;
+        }
+    }
+    return resultado;
 }
 
+
+
+
+
+
+
+
+
+} //Fin clas APP
+
 let app = new App();
+
+console.log(app.convertir(1999));
+console.log(app.convertir(2000));
